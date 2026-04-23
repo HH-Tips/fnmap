ffbaaddac42078541d8e71c44aac05982ffe89d2
HH-Tips <marco.pati2000@libero.it>
2026-04-23 11:48:24 +0200

    fix: correct symlink path in setup script to use absolute home directory reference

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/HH-Tips/fnmap.git ~/.local/share/fnmap
-ln -s ~/.local/share/fnmap/fnmap .local/bin/fnmap
+ln -s ~/.local/share/fnmap/fnmap ~/.local/bin/fnmap
 
 echo "If 'which fnmap' returns no fnmap found add .local/bin/ to your \$PATH env variable."
